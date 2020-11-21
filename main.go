9a2e40e16cc85a0caf911491f9eb18fa0c2ac08b
mohit <iammohit810@gmail.com>
2020-11-21 18:13:51 +0530

    testing new code

--- main.go
+++ main.go
@@ -9,13 +9,19 @@ import (
 	"github.com/mohit810/streamingcdn/signal"
 )
 
+func init() {
+	// Generate pem file for https
+	signal.GenPem()
+}
+
 func main() {
 	port := flag.Int("port", 8000, "http server port")
 	flag.Parse()
 	r := httprouter.New()
 	signal.HTTPSDPServer(r)
-	err := http.ListenAndServe(":"+strconv.Itoa(*port), r)
-	if err != nil {
-		panic(err)
-	}
+	//err := http.ListenAndServe(":"+strconv.Itoa(*port), r)
+	//if err != nil {
+	//	panic(err)
+	//}
+	panic(http.ListenAndServeTLS(":"+strconv.Itoa(*port), "cert.pem", "key.pem", r))
 }
