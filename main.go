bb317dc29dbfa5bcb2b078ee390a5e0e6c08d389
Gh0sT <iammohit810@gmail.com>
2020-11-23 20:06:55 +0530

    working code for hls conversion

--- main.go
+++ main.go
@@ -9,19 +9,19 @@ import (
 	"github.com/mohit810/streamingcdn/signal"
 )
 
-func init() {
-	// Generate pem file for https
-	signal.GenPem()
-}
+//func init() {
+//	// Generate pem file for https
+//	signal.GenPem()
+//}
 
 func main() {
 	port := flag.Int("port", 8080, "http server port")
 	flag.Parse()
 	r := httprouter.New()
 	signal.HTTPSDPServer(r)
-	//err := http.ListenAndServe(":"+strconv.Itoa(*port), r)
-	//if err != nil {
-	//	panic(err)
-	//}
-	panic(http.ListenAndServeTLS(":"+strconv.Itoa(*port), "cert.pem", "key.pem", r))
+	err := http.ListenAndServe(":"+strconv.Itoa(*port), r)
+	if err != nil {
+		panic(err)
+	}
+	//panic(http.ListenAndServeTLS(":"+strconv.Itoa(*port), "cert.pem", "key.pem", r))
 }
